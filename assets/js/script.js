33f231291edbb2c0fde0204f21291aa8ca423795
Naviganet <dadegi@gmail.com>
2026-06-03 12:59:24 +0200

    Codici lezione W5-D2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
 const overlay = document.querySelector('.overlay');
+const openModal = document.getElementById('openModal');
+const closeModal = document.querySelectorAll('.closeModal');
+
+openModal.addEventListener('click', (e) => {
+	e.preventDefault();
+	overlay.style.display = 'block';
+});
+
+closeModal.forEach(function (btn) {
+	btn.addEventListener('click', (e) => {
+		e.preventDefault();
+		overlay.style.display = 'none';
+	});
+});
